1158bdba8ee3b8e1b5078ef07da25355f73aef4c
liam-g-furey <78392149+liam-g-furey@users.noreply.github.com>
2021-02-03 13:54:02 -0500

    Update index.php
    Co-authored-by: Aidan <aidan.mccarthy12@gmail.com>

--- index.php
+++ index.php
@@ -82,12 +82,9 @@ function getDataPointsFromCSV(csv) {
  
 }
 </script>
-</head>
-<body>
 <div id="chartContainer" style="height: 370px; width: 100%;"></div>
 <script type="text/javascript" src="https://canvasjs.com/assets/script/jquery-1.11.1.min.js"></script>
 <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
-</body>
   <script type="text/javascript" src="js/main.js"></script>
 </body>
 </html>
