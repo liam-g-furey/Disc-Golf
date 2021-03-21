0e79ed8f8e6523ba22902d9b9a62829b4012cde8
aidan-gbm <aidan.mccarthy12@gmail.com>
2021-03-21 22:47:12 +0000

    Fix permissions & deploy

--- deploy.sh
+++ deploy.sh
@@ -10,14 +10,13 @@ fail() {
 }
 
 echo '[+] Pulling most recent changes...'
-#git pull || fail 'git pull failed'
+git pull || fail 'git pull failed'
 
 echo '[+] Clearing old web files'
 rm -r /var/www/disc/* || fail 'Failed to clear files'
 
 echo '[+] Adding new files'
 cp -Rp ./* /var/www/disc/ \
-    && mkdir /var/www/disc/ScriptFiles \
     && sudo chown -R aidan:www-data /var/www/disc \
     && rm /var/www/disc/deploy.sh \
     && rm /var/www/disc/README.md \
@@ -28,6 +27,7 @@ cd /var/www/disc
 python3 Python/genData.py || fail 'Failed to run genData.py'
 #python3 Python/ScoresChart.py || fail 'Failed to run ScoresChart.py'
 #python3 Python/WeightedAveragePerformance.py || fail 'Failed to run WeightedAveragePerformance.py'
+rm -r Python || fail 'Failed to remove Python scripts'
 cd - >/dev/null
 
 echo '[+] Done!'
